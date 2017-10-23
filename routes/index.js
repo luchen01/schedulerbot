7460ee969bce4ad48706a7c3c2a381a36254cd64
Brock <bws219@lehigh.edu>
2017-10-23 15:18:55 -0700

    jimin wants my code

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,12 @@ var express = require('express');
 var router = express.Router();
 var WebClient = require('@slack/client').WebClient;
 var web = new WebClient(token);
-var token = process.env.SLACK_API_TOKEN || '';
+var token = process.env.SLACK_BOT_TOKEN || '';
+var rtm = require('./messages');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log(rtm);
   res.render('index', { title: 'Express' });
 });
 
