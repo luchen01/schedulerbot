55db876d826b1f4646606ed265748fde4e30efcc
borajimin <borajimin@gmail.com>
2017-10-23 14:07:34 -0700

    Simple changes

--- routes/index.js
+++ routes/index.js
@@ -5,17 +5,17 @@ var IncomingWebhook = require('@slack/client').IncomingWebhook;
 var url = process.env.SLACK_WEBHOOK_URL || '';
 
 var webhook = new IncomingWebhook(url);
-webhook.send('Hello there', function(err, header, statusCode, body) {
-  if (err) {
-    console.log('Error:', err);
-  } else {
-    console.log('Received', statusCode, 'from Slack');
-  }
-});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+  webhook.send('Hello there', function(err, header, statusCode, body) {
+    if (err) {
+      console.log('Error:', err);
+    } else {
+      console.log('Received', statusCode, 'from Slack');
+    }
+  });
 });
 
 module.exports = router;
