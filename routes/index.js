2d42b12b0f6eba94533494d67c140faae95bc4ac
luchen01 <luchenpengnyc@gmail.com>
2017-10-23 16:22:20 -0700

    add bot

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,32 @@
 var express = require('express');
 var router = express.Router();
-var IncomingWebhook = require('@slack/client').IncomingWebhook;
-var url = process.env.SLACK_WEBHOOK_URL || '';
-var webhook = new IncomingWebhook(url);
 var axios = require('axios');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-  webhook.send('Hello there', function(err, header, statusCode, body) {
-    if (err) {
-      console.log('Error:', err);
-    } else {
-      console.log('Received', statusCode, 'from Slack');
-    }
-  });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+//   webhook.send('Hello there', function(err, header, statusCode, body) {
+//     if (err) {
+//       console.log('Error:', err);
+//     } else {
+//       console.log('Received', statusCode, 'from Slack');
+//     }
+//   });
+// });
 
 
-router.get('/auth', function(req,res, next){
-  axios.get("https://slack.com/oauth/authorize", {
-    client_id: process.env.SLACK_CLIENT_ID,
-    scope: chat write user
-  })
-})
+
+// router.get('/auth', function(req,res, next){
+//   axios.get("https://slack.com/oauth/authorize", {
+//     params:{
+//     client_id: process.env.SLACK_CLIENT_ID,
+//     scope: "chat write user"
+//   }
+//   })
+//   .then(function(resp){
+//     res.send(resp)
+//   })
+// })
 
 
 module.exports = router;
