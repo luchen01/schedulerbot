f725358d1eb176ca3cc2bf7bcec3df9fa3df39cf
borajimin <borajimin@gmail.com>
2017-10-24 15:51:05 -0700

    Fixed typos.

--- routes/dialog.js
+++ routes/dialog.js
@@ -2,16 +2,17 @@
 var axios = require('axios');
 module.exports = {
   interpretUserMessage(message, sessionId){
-    return axios.get('http://api.dialogflow.com/v1/query', {
+    console.log('Got to interpretUserMessage');
+    return axios.get('https://api.dialogflow.com/v1/query', {
       params: {
         v: '20170712',
         query: message,
         sessionId,
-        timeZone: 'America/Los_Angeles',
+        timezone: 'America/Los_Angeles',
         lang: 'en'
       },
       headers:  {
-        Authorizatioin: `Bearer ${process.env.API_AI_TOKEN}`
+        Authorization: `Bearer ${process.env.API_AI_TOKEN}`
       }
     })
   }
