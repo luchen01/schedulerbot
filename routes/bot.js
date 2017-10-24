c5219f098a3cac31993140c247d7d903b5459b47
borajimin <borajimin@gmail.com>
2017-10-24 10:44:30 -0700

    Google happiness

--- routes/bot.js
+++ routes/bot.js
@@ -1,16 +1,40 @@
+var WebClient = require('@slack/client').WebClient;
 var RtmClient = require('@slack/client').RtmClient;
 var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
-var bot_token = process.env.SLACK_API_TOKEN || '';
-
+var bot_token = process.env.SLACK_BOT_TOKEN || '';
+var dialogflow = require('./dialog');
 var rtm = new RtmClient(bot_token);
-
+var web = new WebClient(bot_token);
+rtm.start();
 //The client will emit an RTM.AUTHENTICATED event on successful connection, with the `rtm.start` payload if you want to cache it
 rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
   console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
 });
+
+function handleDialogflowConvo(message) {
+  dialogflow.interpretUserMessage(message.text, message.user)
+  .then(function(res) {
+    var { data } = res;
+    console.log('DIALOGFLOW RESPONSE', res.data);
+    if (data.result.actionIncomplete) {
+      web.chat.postMessage(message.channel, data.result.fulfillment.speech);
+    } else {
+      web.chat.postMessage(message.channel,
+        `You asked me to remind you to ${data.result.parameters.description} on ${data.result.parameters.date}`);
+    }
+  })
+  .catch(function(err) {
+    console.log('Error sending message to Dialogflow', err);
+  });
+}
+
 rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
-  console.log('Message:', message); //this is no doubt the lamest possible message handler, but you get the idea
-  rtm.sendMessage('Thank you for your message', 'G7NHU1THS');
+  if (! message.user) {
+    console.log('Message send by a bot, ignoring');
+    return;
+  }
+
+  web.chat.postMessage(message.channel, `Hello,
+  I'm Scheduler Bot. Please give me access to your Google Calendar http://localhost:3000/setup?slackId=${message.user}`);
 });
-rtm.start();
