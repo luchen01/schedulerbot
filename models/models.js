e1835151dae05ad188666a7cfa711ab060cfd75e
Brock <bws219@lehigh.edu>
2017-10-24 17:35:29 -0700

    deleted og shit

---
+++ models/models.js
@@ -0,0 +1,85 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const taskSchema = new Schema({
+  subject: {
+    type: String,
+    required: true,
+  },
+  day: {
+    type: Date,
+    required: true,
+  },
+  googleCalEventId: String,
+  requesertId: {
+    type: Schema.ObjectId,
+    ref: 'User',
+  },
+});
+
+const meetingSchema = new Schema({
+  day: {
+    type: Date,
+    required: true,
+  },
+  invitees: {  //WTF
+    type: Array,
+    required: true,
+  },
+  subject: String,
+  location: {
+    latitude: Number,
+    longitude: Number,
+  },
+  meetingLength: String, //WTF
+  googleCalFields: {
+    //some other bullshit
+  },
+  confirmed: Boolean,
+  createdAt: Date,
+  requesterId: { //WTF
+    type: Schema.ObjectId,
+    ref: 'User',
+  },
+});
+
+const userSchema = new Schema({
+  googleCalAccount: {
+    accessToken: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+  },
+  slackId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  slackUsername: String,
+  slackEmail: String,
+  SlackDMIds: Array,
+});
+
+const inviteRequestSchema = new Schema({
+  eventId: {
+    type: Schema.ObjectId,
+    ref: 'Meeting',
+  },
+  inviteeId: {
+    type: Schema.ObjectId,
+    ref: 'User',
+  },
+  requesterId: {
+    type: Schema.ObjectId,
+    ref: 'User'
+  },
+  confirmed: Boolean,
+});
+
+module.exports = {
+  User: mongoose.model('User', userSchema),
+  Task: mongoose.model('Task', taskSchema),
+  Meeting: mongoose.model('Meeting', meetingSchema),
+  InviteRequest: mongoose.model('InviteRequest', inviteRequestSchema),
+};
