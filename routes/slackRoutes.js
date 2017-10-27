a760c6ecf2637ebc308d1c858ed02b79fd29f5d5
borajimin <borajimin@gmail.com>
2017-10-27 15:48:55 -0700

    kk

--- routes/slackRoutes.js
+++ routes/slackRoutes.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express();
 var {Task, User, Meeting, InviteRequest} = require('../models/models');
 var google = require('./googleCal');
+
 router.get('/', function(req, res, next) {
   res.redirect('https://slack.com/oauth/authorize');
 });
@@ -9,20 +10,18 @@ router.get('/', function(req, res, next) {
 router.post('/messagesAction', (req, res) =>{
   const data = JSON.parse(req.body.payload);
   const fieldsArr = data.original_message.attachments[0].fields;
-
   let user;
-  // let event;
-  // console.log(data);
+  console.log(data);
   User.findOne({slackId: data.user.id})
   .then(u => {
     user = u;
-    console.log(user.googleCalAccount.accessToken);
+    console.log(fieldsArr);
     return google.createCalendarEvent(user.googleCalAccount,
       fieldsArr[0].value, fieldsArr[1].value
     )
   })
   .then(event => {
-    console.log(event);
+    console.log("IS this event in slack routes",event);
     let temp;
     switch(data.callback_id) {
       case 'reminder':
